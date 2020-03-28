adaecb6ed027c9a5e342e0cc8d31868d003868e0
hoangtunguyen <hoangtunguyen195@gmail.com>
2020-03-28 08:47:22 +0700

    Initial commit

---
+++ src/main/java/com/project/c2cbooking/C2cbookingApplication.java
@@ -0,0 +1,13 @@
+package com.project.c2cbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2cbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C2cbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/c2cbooking/C2cbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.c2cbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2cbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
