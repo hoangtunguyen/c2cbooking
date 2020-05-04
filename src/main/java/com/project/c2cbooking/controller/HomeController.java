41707b7e838b1f22b2e01b38a828a89e4913502e
hoangtunguyen <hoangtunguyen195@gmail.com>
2020-05-04 16:01:54 +0700

    del author authen

--- src/main/java/com/project/c2cbooking/controller/HomeController.java
+++ src/main/java/com/project/c2cbooking/controller/HomeController.java
@@ -3,30 +3,24 @@ package com.project.c2cbooking.controller;
 import com.project.c2cbooking.request.AuthenticationRequest;
 import com.project.c2cbooking.response.AuthenticationResponse;
 import com.project.c2cbooking.response.RoomResponse;
-import com.project.c2cbooking.service.MyUserDetailService;
 import com.project.c2cbooking.service.RoomService;
 import com.project.c2cbooking.service.imp.RoomServiceImp;
-import com.project.c2cbooking.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class HomeController {
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private MyUserDetailService userDetailService;
-
-    @Autowired
-    private JwtUtil  jwtUtil;
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
+//
+//    @Autowired
+//    private MyUserDetailService userDetailService;
+//
+//    @Autowired
+//    private JwtUtil  jwtUtil;
     @Autowired
     private RoomServiceImp roomServiceImp;
     @GetMapping("/")
@@ -48,17 +42,17 @@ public class HomeController {
     }
 
 
-    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws  Exception{
-        try{
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-            );
-        }catch (BadCredentialsException e){
-            throw  new Exception("Incorrect username or password", e);
-        }
-        final UserDetails userDetails = userDetailService.loadUserByUsername(authenticationRequest.getUsername());
-        final String jwt = jwtUtil.generateToken(userDetails);
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
-    }
+//    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+//    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws  Exception{
+//        try{
+//            authenticationManager.authenticate(
+//                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+//            );
+//        }catch (BadCredentialsException e){
+//            throw  new Exception("Incorrect username or password", e);
+//        }
+//        final UserDetails userDetails = userDetailService.loadUserByUsername(authenticationRequest.getUsername());
+//        final String jwt = jwtUtil.generateToken(userDetails);
+//        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+//    }
 }
