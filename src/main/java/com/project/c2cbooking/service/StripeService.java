3b6fce39de04ebb2d44bf129e0bc5e343cb7f3b0
hoangtunguyen <hoangtunguyen195@gmail.com>
2020-05-10 09:45:33 +0700

    stripe

---
+++ src/main/java/com/project/c2cbooking/service/StripeService.java
@@ -0,0 +1,40 @@
+package com.project.c2cbooking.service;
+
+import com.project.c2cbooking.request.ChargeRequest;
+import com.stripe.Stripe;
+import com.stripe.exception.*;
+import com.stripe.model.Charge;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.stripe.Stripe;
+
+import com.stripe.exception.CardException;
+import com.stripe.exception.InvalidRequestException;
+import com.stripe.model.Charge;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+@Service
+public class StripeService {
+
+    @Value("${STRIPE_SECRET_KEY}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+    public Charge charge(ChargeRequest chargeRequest)
+            throws StripeException {
+        Map<String, Object> chargeParams = new HashMap<>();
+        chargeParams.put("amount", chargeRequest.getAmount());
+        chargeParams.put("currency", chargeRequest.getCurrency());
+        chargeParams.put("description", chargeRequest.getDescription());
+        chargeParams.put("source", chargeRequest.getStripeToken());
+        chargeParams.put("receipt_email", "charliehoang195@gmail.com");
+        return Charge.create(chargeParams);
+    }
+}
